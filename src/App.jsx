e7a6104818c7a56b5e7a1470163dd1828ed7d1eb
Abdujalil001 <abdujalilabduvahobov38@gmail.com>
2025-07-31 10:44:45 +0500

    Modalga inputlar qo‘shildi va yangi kitob qo‘shish funksiyasi yaratildi

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,6 @@
 import { useState } from 'react'
 import './App.css'
 import { Button, Modal } from 'antd'
-import img1 from './assets/1.png'
 import img2 from './assets/2.png'
 import img3 from './assets/3.png'
 import img4 from './assets/4.png'
@@ -10,60 +9,68 @@ import img6 from './assets/6.png'
 import img7 from './assets/7.png'
 import imgMain from './assets/image.png'
 
-const books = [
-  {
-    title: 'Tafsiri Hilol',
-    author: 'Shayx Muhammad Sodiq Muhammad Yusuf',
-    img: imgMain,
-    desc: 'Roman XIX asr voqealarini o‘z ichiga olgan...',
-  },
-  {
-    title: 'Ulamolar Nazdida Vaqtning Qadri',
-    author: 'Abdulfattoh Abu G‘udda',
-    img: img2,
-    desc: 'Roman XIX asr voqealarini o‘z ichiga olgan...',
-  },
-  {
-    title: 'Ikki Eshik Orasi',
-    author: 'Abdulla Qodiriy',
-    img: img3,
-    desc: 'Roman XIX asr voqealarini o‘z ichiga olgan...',
-  },
-  {
-    title: 'O‘tkan Kunlar',
-    author: 'Abdulla Qodiriy',
-    img: img4,
-    desc: 'Roman XIX asr voqealarini o‘z ichiga olgan...',
-  },
-  {
-    title: 'Oltin Silsila',
-    author: 'Shayx Muhammad Sodiq Muhammad Yusuf',
-    img: img5,
-    desc: 'Roman XIX asr voqealarini o‘z ichiga olgan...',
-  },
-  {
-    title: 'Yangi Kitob',
-    author: 'Yangi Muallif',
-    img: img6,
-    desc: 'Yangi kitob uchun qisqacha tavsif.',
-  },
-  {
-    title: 'Boshqa Kitob',
-    author: 'Boshqa Muallif',
-    img: img7,
-    desc: 'Boshqa kitob uchun qisqacha tavsif.',
-  },
-]
-
 function App() {
   const [isModalOpen, setIsModalOpen] = useState(false)
+  const [books, setBooks] = useState([
+    {
+      title: 'Tafsiri Hilol',
+      author: 'Shayx Muhammad Sodiq Muhammad Yusuf',
+      img: imgMain,
+      desc: 'Roman XIX asr voqealarini o‘z ichiga olgan...',
+    },
+    {
+      title: 'Ulamolar Nazdida Vaqtning Qadri',
+      author: 'Abdulfattoh Abu G‘udda',
+      img: img2,
+      desc: 'Roman XIX asr voqealarini o‘z ichiga olgan...',
+    },
+    {
+      title: 'Ikki Eshik Orasi',
+      author: 'Abdulla Qodiriy',
+      img: img3,
+      desc: 'Roman XIX asr voqealarini o‘z ichiga olgan...',
+    },
+    {
+      title: 'O‘tkan Kunlar',
+      author: 'Abdulla Qodiriy',
+      img: img4,
+      desc: 'Roman XIX asr voqealarini o‘z ichiga olgan...',
+    },
+    {
+      title: 'Oltin Silsila',
+      author: 'Shayx Muhammad Sodiq Muhammad Yusuf',
+      img: img5,
+      desc: 'Roman XIX asr voqealarini o‘z ichiga olgan...',
+    },
+    {
+      title: 'Yangi Kitob',
+      author: 'Yangi Muallif',
+      img: img6,
+      desc: 'Yangi kitob uchun qisqacha tavsif.',
+    },
+    {
+      title: 'Boshqa Kitob',
+      author: 'Boshqa Muallif',
+      img: img7,
+      desc: 'Boshqa kitob uchun qisqacha tavsif.',
+    },
+  ])
+
+  const [newBook, setNewBook] = useState({
+    title: '',
+    author: '',
+    img: '',
+    desc: '',
+  })
 
   const showModal = () => {
     setIsModalOpen(true)
   }
 
   const handleOk = () => {
+    setBooks([...books, newBook])
     setIsModalOpen(false)
+    setNewBook({ title: '', author: '', img: '', desc: '' })
   }
 
   const handleCancel = () => {
@@ -84,7 +91,33 @@ function App() {
         onCancel={handleCancel}
         closable={{ 'aria-label': 'Close modal' }}
       >
-       <input type="text" name="" id="" placeholder='Kitob nomi'/>
+        <input
+          type="text"
+          placeholder="Kitob nomi"
+          className="modal-input"
+          value={newBook.title}
+          onChange={(e) => setNewBook({ ...newBook, title: e.target.value })}
+        />
+        <input
+          type="text"
+          placeholder="Muallif"
+          className="modal-input"
+          value={newBook.author}
+          onChange={(e) => setNewBook({ ...newBook, author: e.target.value })}
+        />
+        <input
+          type="text"
+          placeholder="Rasm URL"
+          className="modal-input"
+          value={newBook.img}
+          onChange={(e) => setNewBook({ ...newBook, img: e.target.value })}
+        />
+        <textarea
+          placeholder="Tavsif"
+          className="modal-input"
+          value={newBook.desc}
+          onChange={(e) => setNewBook({ ...newBook, desc: e.target.value })}
+        />
       </Modal>
 
       <div className="books-list">
